d7c7c1fe96b93de29ee699b855adc22c227985da
Oleg Churilo <46841358+Kezzzia@users.noreply.github.com>
2019-08-02 08:52:32 +0300

    price almost ready

---
+++ fx/JS/index.js
@@ -0,0 +1,87 @@
+function viewPriceSamsung(){
+    document.getElementById("price__samsung").style.display = "block";
+    document.getElementById("price__apple").style.display = "none";
+    document.getElementById("price__sony").style.display = "none";
+    document.getElementById("price__LG").style.display = "none";
+    document.getElementById("price__xiaomi").style.display = "none";
+    document.getElementById("price__meizu").style.display = "none";
+    document.getElementById("price__huawei").style.display = "none";
+    document.getElementById("price__lenovo").style.display = "none";
+};
+
+function viewPriceApple(){
+    document.getElementById("price__apple").style.display = "block";
+    document.getElementById("price__samsung").style.display = "none";
+    document.getElementById("price__sony").style.display = "none";
+    document.getElementById("price__LG").style.display = "none";
+    document.getElementById("price__xiaomi").style.display = "none";
+    document.getElementById("price__meizu").style.display = "none";
+    document.getElementById("price__huawei").style.display = "none";
+    document.getElementById("price__lenovo").style.display = "none";
+};
+
+function viewPriceSony(){
+    document.getElementById("price__sony").style.display = "block";
+    document.getElementById("price__samsung").style.display = "none";
+    document.getElementById("price__apple").style.display = "none";
+    document.getElementById("price__LG").style.display = "none";
+    document.getElementById("price__xiaomi").style.display = "none";
+    document.getElementById("price__meizu").style.display = "none";
+    document.getElementById("price__huawei").style.display = "none";
+    document.getElementById("price__lenovo").style.display = "none";
+};
+
+function viewPriceLG(){
+    document.getElementById("price__LG").style.display = "block";
+    document.getElementById("price__samsung").style.display = "none";
+    document.getElementById("price__apple").style.display = "none";
+    document.getElementById("price__sony").style.display = "none";
+    document.getElementById("price__xiaomi").style.display = "none";
+    document.getElementById("price__meizu").style.display = "none";
+    document.getElementById("price__huawei").style.display = "none";
+    document.getElementById("price__lenovo").style.display = "none";
+};
+
+function viewPriceXiaomi(){
+    document.getElementById("price__xiaomi").style.display = "block";
+    document.getElementById("price__samsung").style.display = "none";
+    document.getElementById("price__apple").style.display = "none";
+    document.getElementById("price__sony").style.display = "none";
+    document.getElementById("price__LG").style.display = "none";
+    document.getElementById("price__meizu").style.display = "none";
+    document.getElementById("price__huawei").style.display = "none";
+    document.getElementById("price__lenovo").style.display = "none";
+};
+
+function viewPriceMeizu(){
+    document.getElementById("price__meizu").style.display = "block";
+    document.getElementById("price__samsung").style.display = "none";
+    document.getElementById("price__apple").style.display = "none";
+    document.getElementById("price__sony").style.display = "none";
+    document.getElementById("price__LG").style.display = "none";
+    document.getElementById("price__xiaomi").style.display = "none";
+    document.getElementById("price__huawei").style.display = "none";
+    document.getElementById("price__lenovo").style.display = "none";
+};
+
+function viewPriceHuawei(){
+    document.getElementById("price__huawei").style.display = "block";
+    document.getElementById("price__samsung").style.display = "none";
+    document.getElementById("price__apple").style.display = "none";
+    document.getElementById("price__sony").style.display = "none";
+    document.getElementById("price__LG").style.display = "none";
+    document.getElementById("price__xiaomi").style.display = "none";
+    document.getElementById("price__meizu").style.display = "none";
+    document.getElementById("price__lenovo").style.display = "none";
+};
+
+function viewPriceLenovo(){
+    document.getElementById("price__lenovo").style.display = "block";
+    document.getElementById("price__samsung").style.display = "none";
+    document.getElementById("price__apple").style.display = "none";
+    document.getElementById("price__sony").style.display = "none";
+    document.getElementById("price__LG").style.display = "none";
+    document.getElementById("price__xiaomi").style.display = "none";
+    document.getElementById("price__meizu").style.display = "none";
+    document.getElementById("price__huawei").style.display = "none";
+};
