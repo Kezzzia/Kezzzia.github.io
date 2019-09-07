ed06aeee306d412fce4e05a1b43b2dba284b46bf
Oleg Churilo <Oleg.Churilo.job@gmail.com>
2019-09-07 23:06:04 +0300

    almost ready menu

--- fix_pls_god/js/index.js
+++ fix_pls_god/js/index.js
@@ -15,28 +15,7 @@ function init(){
 	map.geoObjects.add(placemark);
 }
 
-function burgerMenu(selector){
-	let menu = $(selector);
-	let button = menu.find(".burger-menu__button");
-	let links = menu.find(".burger-menu__link");
-	let overlay = menu.find(".burger-menu__overlay");
-
-	button.on("click", (e) => {
-		e.preventDefault();
-		toggleMenu();
-	});
-
-	links.on("click", () => toggleMenu());
-	overlay.on("click", () => toggleMenu());
-
-	function toggleMenu() {
-		menu.toggleClass("burger-menu_active");
-
-		if(menu.hasClass("burger-menu_active")){
-			$("body").css("overflow", "hidden");
-		} else {
-			$("body").css("overflow", "visible");
-		}
-	}
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    x.classList.toggle("responsive");
 }
-burgerMenu (".burger-menu");
