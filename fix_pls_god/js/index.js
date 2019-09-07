59f23c819c3ecced224b88a7757017649f7d0516
Oleg Churilo <Oleg.Churilo.job@gmail.com>
2019-09-08 00:54:04 +0300

    ready menu

--- fix_pls_god/js/index.js
+++ fix_pls_god/js/index.js
@@ -19,3 +19,23 @@ function myFunction() {
     var x = document.getElementById("myTopnav");
     x.classList.toggle("responsive");
 }
+
+
+document.getElementById('close__nav-main').onclick = function() {
+	document.getElementById('myTopnav').classList.remove('responsive');
+}
+document.getElementById('close__nav-about').onclick = function() {
+	document.getElementById('myTopnav').classList.remove('responsive');
+}
+document.getElementById('close__nav-price').onclick = function() {
+	document.getElementById('myTopnav').classList.remove('responsive');
+}
+document.getElementById('close__nav-service').onclick = function() {
+	document.getElementById('myTopnav').classList.remove('responsive');
+}
+document.getElementById('close__nav-reviews').onclick = function() {
+	document.getElementById('myTopnav').classList.remove('responsive');
+}
+document.getElementById('close__nav-footer').onclick = function() {
+	document.getElementById('myTopnav').classList.remove('responsive');
+}
