862c5b910f840cd537bfad0958eca24ea686a8a0
Oleg Churilo <Oleg.Churilo.job@gmail.com>
2019-10-01 21:52:34 +0300

    fhdskfhskd

--- fix_pls_god/js/flex.js
+++ fix_pls_god/js/flex.js
@@ -15,9 +15,16 @@ init();
 function showTabs(){
     let data =  this.getAttribute('data');
     let tabBody = document.querySelectorAll('.tab-body');
+    let tab = document.querySelectorAll('.tab');
 
     for (let i = 0; i < tabBody.length; i++){
         tabBody[i].style.display = 'none';
     }
     document.querySelector(`.tab-body[data = "${data}"]`).style.display = 'block';
+
+    for (let f = 0; f < tab.length; f++){
+        tab[f].style.cssText = 'color: #C4C4C4; background-color: #504C58';
+    }
+    document.querySelector(`.tab[data = "${data}"]`).style.cssText = 'color: #504C58; background-color: #D4E1F4';
 }
+
