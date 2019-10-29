6f646e4c7c213a21cc60015061c234d7a37cf31c
Oleg Churilo <Oleg.Churilo.job@gmail.com>
2019-10-29 22:01:19 +0300

    ffsdfs

--- aaa.js
+++
@@ -1,5 +0,0 @@
-    while(true){
-    if (car = 'Renault Logan' === 'Renault Logan'){
-      console.log('we are in Vitebsk');
-    }
-}
--- burger js/script.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    x.classList.toggle("responsive");
-}
